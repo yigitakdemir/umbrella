a8c9128078063fa67c926d63dadd62edd7089b97
yigitakdemir <146138358+yigitakdemir@users.noreply.github.com>
2024-10-17 01:14:54 +0000

    3w son

--- umbrella.rb
+++ umbrella.rb
@@ -1,18 +1,18 @@
 require "http"
 require "json"
 require "dotenv/load"
-
+#hoca puts kullanmis pp yerine
 pp "howdy"
 
 pp "Where are you located?"
 
-user_response = gets.chomp.gsub(" ", "%20")
+user_response = gets.chomp
 
 pp "Checking the weather at " + user_response + "."
 
 gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_response + "&key=" + ENV.fetch("GMAPS_KEY")
 
-raw_response = HTTP.get(gmaps_url).to_s
+raw_response = HTTP.get(gmaps_url)
 
 parsed_response = JSON.parse(raw_response)
 #hoca bu asagidakini tek tek acti
@@ -22,7 +22,7 @@ lng = coords.fetch("lng")
 
 pp user_response + " is at " + lat.round(2).to_s + ", " + lng.round(2).to_s + "."
 
-pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER_API_KEY")
+pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER_KEY")
 
 pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_api_key + "/" + lat.to_s + "," + lng.to_s
 
@@ -31,23 +31,30 @@ raw_response_pirate = HTTP.get(pirate_weather_url)
 parsed_response_pirate = JSON.parse(raw_response_pirate)
 
 currently_hash = parsed_response_pirate.fetch("currently")
-hourly_hash = parsed_response_pirate.fetch("hourly")
 current_temp = currently_hash.fetch("temperature")
+
+hourly_hash = parsed_response_pirate.fetch("hourly")
 hourly_summary = hourly_hash.fetch("data").at(0).fetch("summary")
+
 pp "The current temperature is " + current_temp.to_s + "°F, " + ((current_temp - 32) * 5 / 9).round(2).to_s + "°C."
+#burada hoca minutely summary kullanmis asagida, we sadece var ise bu data (.fetch("minutely",false), sonra onun uzerine if minutely_hash, minutely_hash.fetch("summary"))
 pp "Weather for the next hour is " + hourly_summary.to_s + "."
 
 hourly_precip = Array.new
-0.upto(11) do |hour|
+1.upto(12) do |hour|
   hourly_precip.push(hourly_hash.fetch("data").at(hour).fetch("precipProbability"))
 end
 
 hour = 1
 hourly_precip.each do |precip|
-  pp "In #{hour} hour(s), there is a #{(precip * 100).to_i}% change of precipitation."
-  hour = hour + 1
+  if precip > 0.10
+    pp "In #{hour} hour(s), there is a #{(precip * 100).to_i}% change of precipitation."
+  end
+    hour = hour + 1
 end
 
 if hourly_precip.max > 0.10
   pp "Would recommend carrying an umbrella!"
+else
+  pp "No need for umbrella, most likely."
 end
